ea3c9bf5d2101cf8ba09b42e8b3ee1042a0b937e
Micah C <kenzan@mailbox.org>
2025-01-01 22:30:07 -0500

    Removed uefi checker

--- ryzen/nixos/modules/bootloader.nix
+++ ryzen/nixos/modules/bootloader.nix
@@ -15,16 +15,8 @@
     fsType = "vfat";
   };
 
-
-
   # Kernel parameters for boot customization
   boot.kernelParams = [ "loglevel=4" "quiet" ];
 
-  # Assertions to ensure the system is UEFI-only
-  assertions = [
-    { assertion = builtins.pathExists "/sys/firmware/efi";
-      message = "This system requires UEFI firmware to boot.";
-    }
-  ];
 }
 
