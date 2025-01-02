210f3091361e9c09d60a6b64dd94328cb3acf569
Micah C <kenzan@mailbox.org>
2025-01-02 18:41:35 -0500

    Tweaked as per ChatGPT lol

--- ryzen/nixos/modules/disk-config.nix
+++ ryzen/nixos/modules/disk-config.nix
@@ -1,21 +1,25 @@
 {
   disko.devices = {
     disk = {
-      main = {
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            efi = {
-              start = "0";
-              size = "512MiB";
-              type = "efi";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          efi = {
+            start = "0";
+            size = "512MiB";
+            type = "efi";
+            content = {
+              type = "filesystem";
               format = "vfat";
               mountPoint = "/boot/efi";
             };
-            root = {
-              start = "512MiB";
-              type = "primary";
+          };
+          root = {
+            start = "512MiB";
+            type = "primary";
+            content = {
+              type = "filesystem";
               format = "ext4";
               mountPoint = "/";
             };
