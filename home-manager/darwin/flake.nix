8f1b75f074eead3882b6daf87024dd970f35bddd
Micah C <kenzan@mailbox.org>
2025-01-02 22:24:19 -0500

    Merged nix-darwin home-manager config

---
+++ home-manager/darwin/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Darwin configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    agenix.url = "github:ryantm/agenix";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, darwin, agenix, ... }: {
+    darwinConfigurations = {
+      haruka = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          #./configuration.nix
+          ./darwin.nix
+          agenix.nixosModules.default
+          {
+            environment.systemPackages = [ agenix.packages.aarch64-darwin.default ];
+          }
+          home-manager.darwinModules.home-manager
+          {
+            users.users.micah.home = "/Users/micah";
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.micah = import ./home.nix;
+            
+            # Optionally, use home-manager.extraSpecialArgs to pass
+
+            # arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home-manager/darwin/home.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+
+{
+  home.stateVersion = "24.11";
+
+  home.packages = [
+    pkgs.tmux
+    pkgs.vim
+    pkgs.utm
+    pkgs.btop
+    pkgs.zsh
+  ];
+
+  home.homeDirectory = builtins.toPath "/Users/micah";
+  home.username = "micah";
+
+  programs.home-manager.enable = true;
+
+  home.file.".ssh/config".text = ''
+    Host *
+      ForwardAgent no
+      AddKeysToAgent no
+      Compression no
+      ServerAliveInterval 0
+      ServerAliveCountMax 3
+      HashKnownHosts no
+      UserKnownHostsFile /dev/null
+      StrictHostKeyChecking no
+      ControlMaster no
+      ControlPath ~/.ssh/master-%r@%n:%p
+      ControlPersist no
+  '';
+
+  # Zsh
+  ## Link the actual zshrc from the Nix folder
+
+  home.file.".zshenv".source = ./dotfiles/zshenv;
+  home.file.".zshrc".source = ./dotfiles/zshrc;
+  home.file.".zsh_plugins.txt".source = ./dotfiles/zsh_plugins.txt;
+
+  ## Aliases
+  programs.zsh = {
+      enable = true;
+      shellAliases = {
+          switch = "darwin-rebuild switch --flake ~/.config/nix";
+      };
+  };
+
+}
+
