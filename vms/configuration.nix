88d446ad697b2fffd2d1c433f36486240a2a125d
micah <micah@micah.com>
2025-01-03 20:47:18 -0500

    added env-vars

--- vms/configuration.nix
+++ vms/configuration.nix
@@ -4,26 +4,50 @@
     pkgs,
     ...
 }:
+
+let
+  envVars = import ./env-vars.nix; 
+in
+
 {
     imports = [
         (modulesPath + "/installer/scan/not-detected.nix")
         (modulesPath + "/profiles/qemu-guest.nix")
         ./disk-config.nix
     ];
+
     boot.loader.grub = {
         efiSupport = true;
         efiInstallAsRemovable = true;
     };
+
     services.openssh.enable = true;
 
-    environment.systemPackages = map lib.lowPrio [
-        pkgs.curl
-        pkgs.gitMinimal
-    ];
+    nixpkgs.config.allowUnfree = true;
 
-    users.users.root.openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWawqBRIqOsbk+AElEiR9BDggm2vSnwG5oH3l+h7NYc micah@pc"
-    ];
+    users = {
+        groups.${envVars.adminUser} = {};
+
+        users.${envVars.adminUser} = {
+            isSystemUser = true;
+            shell = pkgs.zsh;
+            extraGroups = [ "wheel" ];
+            #hashedPassword = "${secretVars.adminPassword}";	
+            hashedPassword = "$6$ytTUaT6RAM9I75hu$nzRy./0A7Xz8EiUG1Bn/PBQvSS5Ng7Ui5STlm0Yt1keVFh2d2tjYZjuYGmoY36irrAZdDoEDjDoQRdPT0vNb9/";
+            uid = 1000;
+            group = "${envVars.adminUser}";
+            openssh.authorizedKeys.keys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWawqBRIqOsbk+AElEiR9BDggm2vSnwG5oH3l+h7NYc micah@pc"
+            ];
+        };
+
+        users.${envVars.rootlessDockerUser} = {
+            isNormalUser = true;
+            shell = pkgs.bash;
+            extraGroups = [ "video" "render" ];
+            uid = 1001;
+        };
+    };
 
     system.stateVersion = "24.05";
 }
---
+++ vms/env-vars.nix
@@ -0,0 +1,9 @@
+{
+    zfsSolidStatePool = "storage-ssd";
+
+    rootlessDockerUser = "container";
+
+    systemHostname = "ryzen";
+
+    adminUser = "micah";  
+}
